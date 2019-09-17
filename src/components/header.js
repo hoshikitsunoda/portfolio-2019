9c235d34b9de7f98ec27df7962bb5a16d807bba7
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-16 21:56:37 -0700

    create Header component #1

--- src/components/header.js
+++ src/components/header.js
@@ -2,20 +2,11 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import styled from "styled-components"
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <Wrapper>
+    <Container>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -27,9 +18,20 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
-    </div>
-  </header>
+    </Container>
+  </Wrapper>
 )
+
+const Wrapper = styled.header`
+  background-color: #ffd64d;
+  box-shadow: 0 0.1rem 0.3rem rgba(0, 0, 0, 0.5);
+`
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
 
 Header.propTypes = {
   siteTitle: PropTypes.string,

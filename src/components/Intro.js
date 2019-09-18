008950d577876826716804f0457e546312309cab
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-18 02:41:25 -0700

    added profile photo #3

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
-// import styled from "styled-components"
+import styled from "styled-components"
 
 const Intro = () => {
   const data = useStaticQuery(graphql`
@@ -18,12 +18,33 @@ const Intro = () => {
   `)
   return (
     <div>
-      <Img
-        fixed={data.profile.childImageSharp.fixed}
-        alt="Gatsby Docs are awesome"
-      />
+      <ProfileImg fixed={data.profile.childImageSharp.fixed} alt="Profile" />
+      <TextWrapper>
+        <p>
+          My name is Hosh’ki Tsunoda. <br></br>Front End Developer. UI Designer.
+          <b></b>Based in Orange County, CA.
+        </p>
+      </TextWrapper>
     </div>
   )
 }
+
+const ProfileImg = styled(Img)`
+  display: block !important;
+  margin: 0 auto -1rem;
+`
+
+const TextWrapper = styled.div`
+  background-color: rgba(77, 77, 77, 0.3);
+  text-align: center;
+  padding: 1.8rem 1rem 0.8rem;
+
+  p {
+    color: #fff;
+    font-size: 1.06rem;
+    font-family: "Manjari", sans-serif;
+    margin-bottom: 0;
+  }
+`
 
 export default Intro

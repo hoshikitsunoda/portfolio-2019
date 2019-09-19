c9727eca44b7ecf0320c18ffcbe22fe01e902307
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-18 23:43:45 -0700

    added image query #7

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,7 +6,19 @@ import styled from "styled-components"
 import BackgroundImage from "gatsby-background-image"
 
 const Resume = () => (
-
+  <StaticQuery
+    query={graphql`
+      query {
+        file(relativePath: { eq: "blurred-resume.png" }) {
+          childImageSharp({
+            fluid(quality: 100, maxWidth: 500) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          })
+        }
+      }
+    `}
+  />
 )
 
 export default Resume

800cf2d6adc4e9b1b1f4271647e7557e12fa79cd
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-18 23:57:17 -0700

    add blurred resume image #7

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -9,16 +9,41 @@ const Resume = () => (
   <StaticQuery
     query={graphql`
       query {
-        file(relativePath: { eq: "blurred-resume.png" }) {
-          childImageSharp({
-            fluid(quality: 100, maxWidth: 500) {
+        mobile: file(relativePath: { eq: "blurred-resume.png" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 500) {
               ...GatsbyImageSharpFluid_withWebp
             }
-          })
+          }
         }
       }
     `}
+    render={data => {
+      const imageData = data.mobile.childImageSharp.fluid
+      return (
+        <StyledBackground
+          Tag="section"
+          fluid={imageData}
+          backgroundColor={`#FFD64D`}
+        >
+          <Overlay></Overlay>
+        </StyledBackground>
+      )
+    }}
   />
 )
+
+const StyledBackground = styled(BackgroundImage)`
+  width: 100%;
+  height: 100%;
+  background-position: top center;
+  background-repeat: no-repeat;
+  background-size: contain;
+`
+
+const Overlay = styled.div`
+  width: 100%;
+  height: 100vh;
+`
 
 export default Resume

af8840ec971eff55b77fd8123bd3b19f03c312a6
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-18 23:43:17 -0700

    created Resume component #7

---
+++ src/components/Resume.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { graphql, StaticQuery } from "gatsby"
+
+import styled from "styled-components"
+
+import BackgroundImage from "gatsby-background-image"
+
+const Resume = () => (
+
+)
+
+export default Resume

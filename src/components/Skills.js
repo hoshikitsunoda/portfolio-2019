239e87bdf3d69187d618a58ab8e69b258149d9a3
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-18 22:51:55 -0700

    User can see Skills section in mobile view #5

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,56 +7,58 @@ import styled from "styled-components"
 const Skills = () => {
   const data = useStaticQuery(graphql`
     query {
-      javascriptIcon: file(relativePath: { eq: "icon-javascript.png" }) {
+      javascriptIcon: file(relativePath: { eq: "skills/icon-javascript.png" }) {
         childImageSharp {
           fixed(width: 28, height: 28) {
             ...GatsbyImageSharpFixed
           }
         }
       }
-      reactIcon: file(relativePath: { eq: "icon-react.png" }) {
+      reactIcon: file(relativePath: { eq: "skills/icon-react.png" }) {
         childImageSharp {
           fixed(width: 28, height: 28) {
             ...GatsbyImageSharpFixed
           }
         }
       }
-      nodejsIcon: file(relativePath: { eq: "icon-nodejs.png" }) {
+      nodejsIcon: file(relativePath: { eq: "skills/icon-nodejs.png" }) {
         childImageSharp {
           fixed(width: 28, height: 28) {
             ...GatsbyImageSharpFixed
           }
         }
       }
-      mongodbIcon: file(relativePath: { eq: "icon-mongoDB.png" }) {
+      mongodbIcon: file(relativePath: { eq: "skills/icon-mongoDB.png" }) {
         childImageSharp {
           fixed(width: 28, height: 28) {
             ...GatsbyImageSharpFixed
           }
         }
       }
-      sassIcon: file(relativePath: { eq: "icon-sass.png" }) {
+      sassIcon: file(relativePath: { eq: "skills/icon-sass.png" }) {
         childImageSharp {
           fixed(width: 28, height: 28) {
             ...GatsbyImageSharpFixed
           }
         }
       }
-      photoshopIcon: file(relativePath: { eq: "icon-photoshop.png" }) {
+      photoshopIcon: file(relativePath: { eq: "skills/icon-photoshop.png" }) {
         childImageSharp {
           fixed(width: 28, height: 28) {
             ...GatsbyImageSharpFixed
           }
         }
       }
-      illustratorIcon: file(relativePath: { eq: "icon-illustrator.png" }) {
+      illustratorIcon: file(
+        relativePath: { eq: "skills/icon-illustrator.png" }
+      ) {
         childImageSharp {
           fixed(width: 28, height: 28) {
             ...GatsbyImageSharpFixed
           }
         }
       }
-      figmaIcon: file(relativePath: { eq: "icon-figma.png" }) {
+      figmaIcon: file(relativePath: { eq: "skills/icon-figma.png" }) {
         childImageSharp {
           fixed(width: 28, height: 28) {
             ...GatsbyImageSharpFixed
@@ -66,26 +68,45 @@ const Skills = () => {
     }
   `)
   return (
-    <Wrapper>
-      <Img fixed={data.javascriptIcon.childImageSharp.fixed} alt="javascript" />
-      <Img fixed={data.reactIcon.childImageSharp.fixed} alt="react" />
-      <Img fixed={data.nodejsIcon.childImageSharp.fixed} alt="nodejs" />
-      <Img fixed={data.mongodbIcon.childImageSharp.fixed} alt="mongodb" />
-      <Img fixed={data.sassIcon.childImageSharp.fixed} alt="sass" />
-      <Img fixed={data.photoshopIcon.childImageSharp.fixed} alt="photoshop" />
-      <Img
-        fixed={data.illustratorIcon.childImageSharp.fixed}
-        alt="illustrator"
-      />
-      <Img fixed={data.figmaIcon.childImageSharp.fixed} alt="figma" />
-    </Wrapper>
+    <Background>
+      <h1>SKILLS:</h1>
+      <IconWrapper>
+        <Img
+          fixed={data.javascriptIcon.childImageSharp.fixed}
+          alt="javascript"
+        />
+        <Img fixed={data.reactIcon.childImageSharp.fixed} alt="react" />
+        <Img fixed={data.nodejsIcon.childImageSharp.fixed} alt="nodejs" />
+        <Img fixed={data.mongodbIcon.childImageSharp.fixed} alt="mongodb" />
+        <Img fixed={data.sassIcon.childImageSharp.fixed} alt="sass" />
+        <Img fixed={data.photoshopIcon.childImageSharp.fixed} alt="photoshop" />
+        <Img
+          fixed={data.illustratorIcon.childImageSharp.fixed}
+          alt="illustrator"
+        />
+        <Img fixed={data.figmaIcon.childImageSharp.fixed} alt="figma" />
+      </IconWrapper>
+    </Background>
   )
 }
 
-const Wrapper = styled.div`
+const Background = styled.div`
+  background-color: rgba(77, 77, 77, 0.3);
+  padding: 0 0.75rem 0.5rem;
+
+  h1 {
+    font-family: "Coda", cursive;
+    font-size: 1.2rem;
+    color: #fff;
+    margin: 0;
+  }
+`
+
+const IconWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding: 1rem 0 0.5rem;
 `
 
 export default Skills

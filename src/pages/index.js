5b9c0acaf8d07f59f5d61872bf0f4257a22439ed
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-20 21:02:01 -0700

    changed Index page to class component #9

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import { graphql, StaticQuery } from "gatsby"
 
 import styled from "styled-components"
@@ -8,42 +8,46 @@ import BackgroundImage from "gatsby-background-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        mobile: file(relativePath: { eq: "bg-image-hoshki-kana.png" }) {
-          childImageSharp {
-            fluid(quality: 90, maxWidth: 500) {
-              ...GatsbyImageSharpFluid_withWebp
+class IndexPage extends Component {
+  render() {
+    return (
+      <StaticQuery
+        query={graphql`
+          query {
+            mobile: file(relativePath: { eq: "bg-image-hoshki-kana.png" }) {
+              childImageSharp {
+                fluid(quality: 90, maxWidth: 500) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+            desktop: file(relativePath: { eq: "bg-desktop-full.png" }) {
+              childImageSharp {
+                fluid(quality: 90, maxWidth: 1440) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
             }
           }
-        }
-        desktop: file(relativePath: { eq: "bg-desktop-full.png" }) {
-          childImageSharp {
-            fluid(quality: 90, maxWidth: 1440) {
-              ...GatsbyImageSharpFluid_withWebp
-            }
-          }
-        }
-      }
-    `}
-    render={data => {
-      const imageData = data.mobile.childImageSharp.fluid
-      return (
-        <StyledBackground
-          Tag="section"
-          fluid={imageData}
-          backgroundColor={`#FFD64D`}
-        >
-          <Layout>
-            <SEO title="Home" />
-          </Layout>
-        </StyledBackground>
-      )
-    }}
-  />
-)
+        `}
+        render={data => {
+          const imageData = data.mobile.childImageSharp.fluid
+          return (
+            <StyledBackground
+              Tag="section"
+              fluid={imageData}
+              backgroundColor={`#FFD64D`}
+            >
+              <Layout>
+                <SEO title="Home" />
+              </Layout>
+            </StyledBackground>
+          )
+        }}
+      />
+    )
+  }
+}
 
 const StyledBackground = styled(BackgroundImage)`
   width: 100%;

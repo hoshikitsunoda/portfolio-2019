20dbbc8ef61b62243d3d5d9e5a78cb2cf6498f63
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-07-30 13:18:11 -0700

    created Projects component

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Project from "./Project/Project"
+
+const Projects = () => {
+  return (
+    <div>
+      <Project />
+      <Project />
+      <Project />
+    </div>
+  )
+}
+
+export default Projects

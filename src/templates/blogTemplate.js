72dd5a3addf7a445a47260b70bcb83852af69905
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-08-11 22:33:21 -0700

    added Layout and SEO to blog post page

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,13 +1,15 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
+import Layout from "../hoc/Layout/layout"
+import SEO from "../components/seo"
+
+export default function Template({ data }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="blog-post-container">
+    <Layout>
+      <SEO title={frontmatter.title} />
       <div className="blog-post">
         <h1>{frontmatter.title}</h1>
         <h2>{frontmatter.date}</h2>
@@ -16,7 +18,7 @@ export default function Template({
           dangerouslySetInnerHTML={{ __html: html }}
         />
       </div>
-    </div>
+    </Layout>
   )
 }
 

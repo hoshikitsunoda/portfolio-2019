953fbccab8d72ff91fde7caef0fb02cc1c930461
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-08-12 00:25:56 -0700

    added theming to the site

---
+++ src/themes/theme.js
@@ -0,0 +1,13 @@
+export default {
+  fonts: {
+    main: "inherit",
+    bold: "Coda, cursive",
+  },
+  colors: {
+    main1: "#ffd64d",
+    main2: "#e3be45",
+    main3: "#ffee55",
+    dark1: "#011a27",
+    opaqueBg: "rgba(0, 0, 0, 0.3)",
+  },
+}

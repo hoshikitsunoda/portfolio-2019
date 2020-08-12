445f285a108c6dbcc7acb647c8109df02ee28cf4
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-08-12 01:30:31 -0700

    updated Blog styling

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -30,16 +30,20 @@ const Blog = () => {
       {allMarkdownRemark.edges.map(({ node }) => {
         return (
           <Panel key={node.frontmatter.slug}>
-            <div>
-              <time dateTime={node.frontmatter.date}>
-                {node.frontmatter.date}
-              </time>
-            </div>
-            <h2>
-              <a href={node.frontmatter.slug}>{node.frontmatter.title}</a>
-            </h2>
-            <p>{node.excerpt}</p>
-            <a href={node.frontmatter.slug}>Read more...</a>
+            <PanelTop>
+              <div>
+                <Time dateTime={node.frontmatter.date}>
+                  {node.frontmatter.date}
+                </Time>
+              </div>
+              <Title>
+                <a href={node.frontmatter.slug}>{node.frontmatter.title}</a>
+              </Title>
+            </PanelTop>
+            <PanelBottom>
+              <Excerpt>{node.excerpt}</Excerpt>
+              <Link href={node.frontmatter.slug}>Read more...</Link>
+            </PanelBottom>
           </Panel>
         )
       })}
@@ -67,11 +71,68 @@ const Wrapper = styled.section`
 const Panel = styled.div`
   flex: 0 0 calc(33.333333% - 1rem);
   margin: 0.5rem;
-  padding: 1rem;
   border-radius: 20px;
   background: ${({ theme }) => theme.colors.main1};
-  box-shadow: inset 17px 17px 25px ${({ theme }) => theme.colors.main2},
-    inset -17px -17px 25px ${({ theme }) => theme.colors.main3};
+  box-shadow: inset 7px 7px 15px ${({ theme }) => theme.colors.main2},
+    inset -7px -7px 15px ${({ theme }) => theme.colors.main3};
+
+  > * {
+    font-family: ${({ theme }) => theme.fonts.main};
+  }
+`
+
+const PanelTop = styled.div`
+  padding: 1rem 1rem 0;
+`
+
+const PanelBottom = styled.div`
+  border-radius: 0 0 20px 20px;
+  padding: 0.5rem 1rem 1rem;
+  background-color: ${({ theme }) => theme.colors.opaqueBg1};
+  box-shadow: 0 -6px 10px -6px ${({ theme }) => theme.colors.opaqueBg1};
+
+  > * {
+    color: ${({ theme }) => theme.colors.textAccent};
+  }
+`
+
+const Time = styled.time`
+  font-size: 0.75rem;
+`
+
+const Title = styled.h2`
+  font-family: ${({ theme }) => theme.fonts.bold};
+  margin: 0.2rem 0 1rem;
+  transition: all 0.2s ease-in;
+
+  > a {
+    color: ${({ theme }) => theme.colors.dark1};
+    text-decoration: none;
+    font-size: 1.25rem;
+    transition: all 0.2s ease-in;
+  }
+
+  &:hover {
+    > a {
+      color: ${({ theme }) => theme.colors.textAccent};
+    }
+  }
+`
+
+const Excerpt = styled.p`
+  margin-bottom: 0.5rem;
+  font-size: 1rem;
+`
+
+const Link = styled.a`
+  color: ${({ theme }) => theme.colors.dark1};
+  text-decoration: none;
+  transition: all 0.2s ease-in;
+  font-size: 0.8rem;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.textAccent};
+  }
 `
 
 export default Blog

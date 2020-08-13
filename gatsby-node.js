12e6d8035b83ec1ad550388a08e06b7eaf80e23a
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-08-12 22:16:25 -0700

    added tags page and updated gatsby node to handle tags

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,14 @@
+const path = require("path")
+
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
 
   const blogPostTemplate = require.resolve(`./src/templates/blogTemplate.js`)
+  const tagsTemplate = path.resolve("src/templates/tagsTemplate.js")
 
   const result = await graphql(`
     {
-      allMarkdownRemark(
+      postsRemark: allMarkdownRemark(
         sort: { order: DESC, fields: [frontmatter___date] }
         limit: 1000
       ) {
@@ -17,21 +20,39 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
           }
         }
       }
+      tagsGroup: allMarkdownRemark(limit: 2000) {
+        group(field: frontmatter___tags) {
+          fieldValue
+        }
+      }
     }
   `)
 
-  // Handle errors
   if (result.errors) {
     reporter.panicOnBuild(`Error while running GraphQL query.`)
     return
   }
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  const posts = result.data.postsRemark.edges
+
+  posts.forEach(({ node }) => {
     createPage({
       path: node.frontmatter.slug,
       component: blogPostTemplate,
       context: {
         slug: node.frontmatter.slug,
+      },
+    })
+  })
+
+  const tags = result.data.tagsGroup.group
+
+  tags.forEach(tag => {
+    createPage({
+      path: `/tags/${tag.fieldValue}/`,
+      component: tagsTemplate,
+      context: {
+        tag: tag.fieldValue,
       },
     })
   })

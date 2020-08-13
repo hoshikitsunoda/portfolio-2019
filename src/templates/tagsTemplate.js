8333de5c809d3b363759db24104d5bfa48bea259
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-08-12 22:15:49 -0700

    created tags template

---
+++ src/templates/tagsTemplate.js
@@ -0,0 +1,52 @@
+import React from "react"
+
+import { Link, graphql } from "gatsby"
+
+const Tags = ({ pageContext, data }) => {
+  console.log(data)
+  const { tag } = pageContext
+  const { edges, totalCount } = data.allMarkdownRemark
+  const tagHeader = `${totalCount} post${
+    totalCount === 1 ? "" : "s"
+  } tagged with "${tag}"`
+
+  return (
+    <div>
+      <h1>{tagHeader}</h1>
+      <ul>
+        {edges.map(({ node }) => {
+          const { title, slug } = node.frontmatter
+          return (
+            <li key={slug}>
+              <Link to={slug}>{title}</Link>
+            </li>
+          )
+        })}
+      </ul>
+      <Link to="/tags">All tags</Link>
+    </div>
+  )
+}
+
+export default Tags
+
+export const pageQuery = graphql`
+  query($tag: String) {
+    allMarkdownRemark(
+      limit: 2000
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { tags: { in: [$tag] } } }
+    ) {
+      totalCount
+      edges {
+        node {
+          frontmatter {
+            slug
+            title
+            tags
+          }
+        }
+      }
+    }
+  }
+`

58cb7a07575bfef253ddd268549b71fdb60ae931
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-08-15 23:01:14 -0700

    added View All link

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 import BlogPost from "./BlogPost/BlogPost"
 
@@ -29,6 +29,12 @@ const Blog = () => {
 
   return (
     <Wrapper className="project-wrapper">
+      <LinkWrapper>
+        <CustomLink to="/blog/list">View All</CustomLink>
+      </LinkWrapper>
+      {allMarkdownRemark.edges.map((props, i) => {
+        return <BlogPost key={i} {...props} />
+      })}
       {allMarkdownRemark.edges.map((props, i) => {
         return <BlogPost key={i} {...props} />
       })}
@@ -43,13 +49,35 @@ const Wrapper = styled.section`
   align-items: flex-start;
   overflow: auto;
   height: 50vh;
-  padding: 1.25rem 1rem;
+  padding: 1.25rem 1rem 5rem;
   margin: 0 auto;
 
   @media (min-width: 768px) {
     height: 65vh;
-    padding: 1.25rem 4rem;
+    padding: 1.25rem 4rem 5rem;
   }
+`
+
+const LinkWrapper = styled.div`
+  position: fixed;
+  padding: 1.5rem 0 3rem;
+  width: 100%;
+  left: 0;
+  bottom: 0;
+  text-align: center;
+  background-color: ${({ theme }) => theme.colors.main1};
+
+  @media (min-width: 768px) {
+    width: 80%;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+`
+
+const CustomLink = styled(Link)`
+  color: ${({ theme }) => theme.colors.dark1};
+  font-family: ${({ theme }) => theme.fonts.bold};
+  text-transform: capitalize;
 `
 
 export default Blog

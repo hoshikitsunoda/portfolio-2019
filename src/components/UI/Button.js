82656e049da08673b88ede68ba231108f5c67e13
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-08-24 00:45:07 -0700

    updated styling

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -3,7 +3,6 @@ import React from "react"
 import styled from "styled-components"
 
 const Button = ({ children, blogList }) => {
-  console.log(blogList)
   return <CustomButton blogList={blogList}>{children}</CustomButton>
 }
 
